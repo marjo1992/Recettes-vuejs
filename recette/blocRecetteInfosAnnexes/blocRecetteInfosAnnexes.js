679280a8a7d4434147095876d9ebafa2e18f83d3
marjo1992 <verelmarjolaine@hotmail.fr>
2021-04-07 13:37:35 +0200

    Fix #32 : Ajout bouton partage, et partage fb et twitter

--- recette/blocRecetteInfosAnnexes/blocRecetteInfosAnnexes.js
+++ recette/blocRecetteInfosAnnexes/blocRecetteInfosAnnexes.js
@@ -32,14 +32,39 @@ Vue.component("blocRecetteInfosAnnexes", {
                     </span>
                 </span>
             </div>
-        </div>		
+        </div>
+        <div>
+            <h2>Partage</h2>
+            <div>
+                <a><span class="icon-partager" v-if="displayShare" @click="partager"></span></a>
+                <span class="fb-share-button" :data-href="urlShare" data-layout="button" data-size="small">
+                    <a target="_blank" :href="fbUrl" class="fb-xfbml-parse-ignore">
+                        <span class="icon-facebook"></span>
+                    </a>
+                </span>
+                <a class="twitter-share-button"
+                    :href="twitterUrl"
+                    data-size="large">
+                    <span class="icon-twitter"></span>
+                </a>
+            </div>
         </div>
     </div>`,
     data() {
+        let urlShare = window.location.href
         return {
+            displayShare: navigator.share,
+            urlShare,
+            fbUrl: "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(urlShare) + "&amp;src=sdkpreparse",
+            twitterUrl: "https://twitter.com/intent/tweet?text=" +  encodeURIComponent(this.recette.nom) + "&url=" + encodeURIComponent(urlShare)
         }
     },
 	methods: {
+		partager() {
+			navigator.share({
+				url: this.urlShare
+			})
+		},
         getListeElem(elemGroupe) {
             return Object.keys(elemGroupe).filter(k => !isNaN(k)).map(k => elemGroupe[k])
         }
